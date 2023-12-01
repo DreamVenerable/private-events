441229c97cc03eef43390a30cddeca02fe0a638e
DreamVenerable <114648602+DreamVenerable@users.noreply.github.com>
2023-12-01 02:10:20 +0200

    user can now attend events

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -6,18 +6,18 @@ class AttendancesController < ApplicationController
   end
 
   def create
-    @attendance = current_user.attendances.build(attended_event_id: attendance_params)
+    @attendance = current_user.attendances.build(attendance_params)
     if @attendance.save
-      redirect_to events_path
+      redirect_to events_path, :notice => 'You are now attending the event.'
     else
       flash.now[:error] = 'Failed to attend the event.'
-      render 'events/show'
+      redirect_to event_path(id: attendance_params.value)
     end
   end
 
   private
 
   def attendance_params
-    params.require(:attendance).permit(:event_id)
+    params.require(:attendance).permit(:attended_event_id)
   end
 end
